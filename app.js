7318f19af3cc3b56b4c8a5430d85b74352194e2d
romina moreno <romina.moreno@fing.edu.uy>
2020-10-24 19:47:24 -0300

    continue integrating

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-const PORT = process.env.PORT || 3000;
- 
-var app = express();
- 
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mpIntegration = require('./mpIntegration');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 3000; 
+
+const app = express();
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', {view:'home'});
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+    let qry = req.query;
+    qry.view = 'item';
+    console.log(JSON.stringify(qry));
+    res.render('detail', qry);
+});
+
+app.get('/payment/success', function (req, res) {
+
+    let qry = req.query;
+    qry.view = '';
+    console.log(JSON.stringify(qry));
+    res.render('success', qry);
+});
+
+app.get('/payment/failure', function (req, res) {
+    let qry = req.query;
+    qry.view = '';
+    console.log(JSON.stringify(qry));
+    res.render('failure', qry);
+});
+
+app.get('/payment/pending', function (req, res) {
+    let qry = req.query;
+    qry.view = '';
+    console.log(JSON.stringify(qry));
+    res.render('pending', qry);
+});
+
+app.post('/payment', (req,res) => mpIntegration.postPreferences(req,res));
+
+app.post('/mp/notification',function(req, res) {
+    console.log(req.body);
+    res.statusCode = 200;
+    res.end("OK");
 });
 
 app.use(express.static('assets'));
---
+++ mpIntegration.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const mercadopago = require('mercadopago');
+const bodyParser = require('body-parser');
+
+const mpIntegration = express.Router();
+mpIntegration.use(bodyParser.urlencoded({ extended: true }));
+
+mercadopago.configure({
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'
+});
+
+module.exports.postPreferences = (req,res) => {
+    console.log(JSON.stringify(req.body));
+    const item = req.body;
+
+    var preference = {
+        items: [
+          {
+            id: '1234',
+            title: item.title,
+            description: '​Dispositivo móvil de Tienda e-commerce​',
+            quantity: parseInt(item.unit),
+            currency_id: 'ARS',
+            unit_price: Number(item.price),
+            picture_url: item.img
+          }
+        ],
+
+        payer: {
+            name: 'Lalo',
+            surname: 'Landa',
+            email: 'test_user_63274575@testuser.com',
+            phone: {
+                    area_code: '11',
+                    number: 22223333
+                },
+            address: {
+                zip_code: '1111',
+                street_name: 'False',
+                street_number: 123
+                }
+        },
+
+        payment_methods: {
+            excluded_payment_methods: [
+                {
+                    id: 'amex'
+                }
+            ],
+            excluded_payment_types: [
+                {
+                    id: 'atm'
+                }
+            ],
+            installments: 6
+        },
+
+        back_urls: {
+            success: "https://rominamoreno9-mp-commerce-node.herokuapp.com/payment/success",
+            failure: "https://rominamoreno9-mp-commerce-node.herokuapp.com/payment/failure",
+            pending: "https://rominamoreno9-mp-commerce-node.herokuapp.com/payment/pending"
+
+        },
+        auto_return: "approved",
+        notification_url: "https://rominamoreno9-mp-commerce-node.herokuapp.com/mp/notification",
+        external_reference: 'rvmoreno9@gmail.com',
+      };
+
+
+    mercadopago.preferences.create(preference).then(response => {
+        if (response.ok || response.status === 201) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            console.log(error);
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        console.log(errmess);
+    })
+    .then(response => {
+        console.log('OK',response);
+        res.redirect(response.body.init_point);
+    })
+    .catch(error => {
+        console.log('Error', error.message);
+    });
+
+};   
